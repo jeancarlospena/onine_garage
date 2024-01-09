8db192a624815be18d76246fdcc9ed2bd1436013
Carlos <jcpacc6@outlook.com>
2024-01-09 08:42:51 -0500

    updated config

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser')
 const path = require('path')
 
 const app = express()
-app.use(cors())
+
 app.use(express.json({ limit: '50mb' }));
 
 // looks up if the request has a body
@@ -19,7 +19,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.json())
 app.use(cookieParser())
 
-
+app.use(cors())
 
 
 // global middleware
