62d8d1cc5a98f5c1517249f51b0c3548f5c6c1a6
Carlos <jcpacc6@outlook.com>
2024-01-09 07:48:17 -0500

    updated config

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.json())
 app.use(cookieParser())
 
+console.log()
+
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://online-garage.onrender.com"],
   credentials: true
 }))
 // global middleware
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const enviroment = process.env.NODE_ENV === 'development' ? "http://localhost:3000" : 'https://online-garage.onrender.com/'
+// const enviroment = process.env.NODE_ENV === 'development' ? "http://localhost:3000" : 'https://online-garage.onrender.com'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api": enviroment,
+      "/api": 'https://online-garage.onrender.com',
     },
   },
   plugins: [react()],
