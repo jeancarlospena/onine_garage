327e5aa912ecddf61a53a30689c7af029b108a8c
Carlos <jcpacc6@outlook.com>
2024-01-08 20:24:03 -0500

    3 commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const enviroment = process.env.NODE_ENV === 'development' ? "http://localhost:3000" : 'https://dashboard.heroku.com'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://dashboard.heroku.com/",
-      // "/api": "http://localhost:3000",
+      "/api": enviroment,
     },
   },
   plugins: [react()],
