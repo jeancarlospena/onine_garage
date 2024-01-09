395caf52f646bb568e7d20aba1eff912843f337e
Carlos <jcpacc6@outlook.com>
2024-01-08 20:15:40 -0500

    2 commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api": "http://localhost:3000",
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": "https://dashboard.heroku.com/",
+      // "/api": "http://localhost:3000",
+    },
+  },
   plugins: [react()],
 })
